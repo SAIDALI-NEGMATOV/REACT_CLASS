4c89f89561ba07f84218f0a47b5ebb9b2344f34d
Saidali Negmatov <114974636+SAYYIDALI2706@users.noreply.github.com>
2023-01-18 19:14:42 +0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,90 @@
+// let obj = {
+//     name:'Saidali',
+//     lastName:"Negmatov",
+//     age:23,
+//     full_name (){
+//         console.log(this.name);
+//     }
+// }
+
+// obj.full_name()
+
+// let a = 12%2 === 0 ? 'juft' : 'toq'
+// console.log(a);
+
+
+// let son = +prompt('sonni kriting')
+
+// let a = son%2 === 0 ? (son%3 === 0 ? 'bu son 2 ga va 3ga bolinadi' : 'faqat 2 ga') : (son%3 === 0 ? 'faqat 3ga': 'hechqaysisiga')
+
+// console.log(a);
+
+
+// class Shop {
+//     name = {
+//         uz:"",
+//         eng:"",
+//         ru:"",
+//     };
+//     created_at = "";
+//     zip_code= "";
+//     from= "";
+//     recipe = [];
+
+//     constructor(name_uz,name_eng,name_ru,created_at,zip_code,from,...recipe) {
+//         this.name.uz = name_uz;
+//         this.name.eng = name_eng;
+//         this.name.ru =name_ru;
+//         this.created_at = created_at;
+//         this.from = from;
+//         this.zip_code = zip_code;
+//         this.recipe = recipe;
+//     }
+// }
+
+// let product1 = new Shop(
+//     "suv",
+//     "water",
+//     "vada",
+//     "11.11.2011",
+//     "family",
+//     "family",
+//     "bakalashka",
+//     "bakalashka",
+//     "bakalashka",
+//     "bakalashka",
+// )
+
+// class Chocalate {
+//     name ={
+//         uz:"",
+//         ru:"",
+//         eng:"",
+//     };
+//     creator = "";
+//     zip_code = "";
+//     from = "";
+//     recipe = "";
+
+//     constructor(name_uz,name_ru,name_eng,creator,zip_code,from,...recipe){
+//         this.name.uz = name_uz;
+//         this.name.ru = name_ru;
+//         this.name.eng = name_eng;
+//         this.creator = creator;
+//         this.zip_code = zip_code;
+//         this.from = from;
+//         this.recipe = recipe;
+//     }
+// }
+
+// let product2 = new Chocalate(
+//     "mone",
+//     "mane",
+//     "mine",
+//     "mone_Consalting",
+//     "#2112211223",
+//     "America",
+//     "milk",
+//     "cacao",
+//     "suger",
+// )
